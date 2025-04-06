1f1bc04cbf6a9079c97b7d0f8648341c50645a20
FernandoTorresEsteche <fate041105@gmail.com>
2025-04-05 21:05:34 -0300

    clase2

---
+++ Clase3/for.py
@@ -0,0 +1,4 @@
+#bucle for
+
+for i in range(6,8,9):
+    print(i)
---
+++ Clase3/listas.py
@@ -0,0 +1,15 @@
+#Practica sobre listas
+
+array = ["futbol","Pc",18.6,18,[6,7,10.4],True,False]
+print(array)
+print(array[1]) 
+print(array[4]) 
+print(array[-1])
+print(array[0:3])
+print(array[:2])
+print(array[2:])
+
+#Cantidad de datos que cuenta el array
+
+print(len(array))
+
