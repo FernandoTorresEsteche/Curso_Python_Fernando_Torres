5ca1c5a150dbda0894a0f0b0a06d470dde94f207
FernandoTorresEsteche <fate041105@gmail.com>
2025-04-10 13:53:30 -0300

    clase3

--- Clase3/for.py
+++ Clase3/for.py
@@ -1,4 +1,8 @@
 #bucle for
 
-for i in range(6,8,9):
-    print(i)
+for i in range(6,8,9,4,7):
+    print("Datos: {i}")
+
+data= [6,8,9,4,7]
+for i in data:
+    print("Datos: {i}")
--- Clase3/listas.py
+++ Clase3/listas.py
@@ -12,4 +12,39 @@ print(array[2:])
 #Cantidad de datos que cuenta el array
 
 print(len(array))
+ 
+#Agregar un valor dentro de la lista
+array.append(66)
+print(array)
+#Insertar datos en una posicion
+array.insert(1,88)
+print(array)
+#Insertar mas de un dato al final
+array.extend([1,88,True,100])
+print(array)
 
+array1 = ["futbol","Pc",18.6,18,[6,7,10.4],True,False,"PC"]
+array2 = [200,250,"hola"]
+array3 = array1 + array2
+print(array3)
+#Buscar valores dentro de un array
+print("PC" in array1)
+#Saber el indice donde esta lo que busco
+print(array1.index("PC"))
+#Cantidad de veces que tengo el valor en mi array
+print(array1.count("PC"))
+#Eliminar valores del array
+array1.remove("PC")
+print(array1)
+#Limpiar
+array1.clear()
+print(array1)
+#Cambiar la posicion
+array1.reverse()
+print(array1)
+#Ordenar de mayor a menor
+array4= [1,2,8,-11,5]
+array4.sort()
+print(array4)
+array4.sort(reverse=True)
+print(array4)
