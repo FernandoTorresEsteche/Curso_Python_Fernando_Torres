7a66f1cc18ec7dc4b30727fbcc3f53a1ac3e9cee
FernandoTorresEsteche <fate041105@gmail.com>
2025-04-19 19:03:08 -0300

    Clase4

---
+++ Clase4/setMain.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hello World!'
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=True, port=5000)
+
+    app.run(debug=True)
